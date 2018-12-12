e85bbdbf359ca9d2ed260de97cd1a1692404cea1
kaleid-liner <32812933+kaleid-liner@users.noreply.github.com>
2018-12-13 00:47:09 +0800

    add Game Over, stop, score display, reset to game

--- fpga-tetris.srcs/sources_1/new/GameDisplayer.v
+++ fpga-tetris.srcs/sources_1/new/GameDisplayer.v
@@ -22,6 +22,7 @@
 
 module GameDisplayer(
     input clk_5mhz,
+    input [2:0] state,
     input [255:0] fallen_blocks,
     input [7:0] xy1, xy2, xy3, xy4,
     output A, B, C, D,
@@ -29,6 +30,24 @@ module GameDisplayer(
     );
     wire [255:0] lattice;
     wire [255:0] current_block_lattice_mask;
+    
+    parameter PLAYING = 0, STOPPING = 1, CLEARING = 2, NOTHING = 3, DROPPING = 4, DEAD = 5;
+    parameter [255:0] GG =      {{16{1'b0}},
+                                {16{1'b0}},
+                                {2{8'b00111000}},
+                                {2{8'b01000100}},
+                                {2{8'b01000100}},
+                                {2{8'b10000100}},
+                                {2{8'b10001110}},
+                                {2{8'b10000000}},
+                                {2{8'b10000000}},
+                                {2{8'b10000000}},
+                                {2{8'b01000100}},
+                                {2{8'b01000100}},
+                                {2{8'b00111100}},
+                                {16{1'b0}},
+                                {16{1'b0}},
+                                {16{1'b0}}};
     
     generate
          genvar i;
@@ -39,7 +58,8 @@ module GameDisplayer(
          end
     endgenerate
     
-    assign lattice = fallen_blocks | current_block_lattice_mask;
+    assign lattice = (state == DEAD) ? GG
+                        : (fallen_blocks | current_block_lattice_mask);
     
     LatticeDisplayer latticeDisplayer (lattice, clk_5mhz, A, B, C, D, G, DI, CLK, LAT);
 endmodule
--- fpga-tetris.srcs/sources_1/new/Tetris.v
+++ fpga-tetris.srcs/sources_1/new/Tetris.v
@@ -27,9 +27,18 @@ module Tetris(
     input left,
     input right,
     input drop,
-    output A, B, C, D, G, DI, CLK, LAT
+    input stop,
+    output A, B, C, D, G, DI, CLK, LAT,
+    output [7:0] an,
+    output [6:0] seg
     );
     reg [255:0] fallen_blocks;
+    
+    // score 
+    reg [3:0] ones_score;
+    reg [3:0] tens_score;
+    reg [3:0] hundreds_score;
+    reg [3:0] thousands_score;
     
     wire clk_5mhz;
     wire clk_1khz;
@@ -40,7 +49,7 @@ module Tetris(
     parameter I = 1, O = 2, T = 3, S = 4, Z = 5, J = 6, L = 7; 
     
     // use state machine to avoid multiple assignment 
-    parameter PLAYING = 0, STOPPING = 1, CLEARING = 2, NOTHING = 3, DROPPING = 4;
+    parameter PLAYING = 0, STOPPING = 1, CLEARING = 2, NOTHING = 3, DROPPING = 4, DEAD = 5;
     reg [2:0] cur_state;
     
     reg [2:0] cur_shape;
@@ -134,8 +143,29 @@ module Tetris(
     
     task ResetGame;
     begin
+    ones_score <= 0;
+    tens_score <= 0;
+    hundreds_score <= 0;
+    thousands_score <= 0;
     fallen_blocks <= 0;
     GenerateNewTetromino();
+    end
+    endtask
+    
+    task GetScore;
+    begin
+    if (ones_score == 9) begin
+        if (tens_score == 9) begin
+            if (hundreds_score == 9) begin
+                if (thousands_score == 9) begin 
+                end
+                else thousands_score <= thousands_score + 1;
+            end
+            else hundreds_score <= hundreds_score + 1;
+        end
+        else tens_score <= tens_score + 1;
+    end
+    else ones_score <= ones_score + 1;
     end
     endtask
     
@@ -146,27 +176,40 @@ module Tetris(
     cur_tlx = 6;
     cur_tly = 0;
     cur_rotate = 0;
+    ones_score = 0;
+    tens_score = 0;
+    hundreds_score = 0;
+    thousands_score = 0;
     end
     
     clk_wiz_0 display_clock (.clk_out1(clk_5mhz), .reset(0), .clk_in1(clk));
     clock_1khz update_clock (clk_5mhz, clk_1khz);
     clock_10hz input_clock (clk_5mhz, clk_10hz);
     
-    GameDisplayer gameDisplayer (clk_5mhz, fallen_blocks, 
+    GameDisplayer gameDisplayer (clk_5mhz, cur_state, fallen_blocks, 
                                  cur_xy1, cur_xy2, cur_xy3, cur_xy4, 
                                  A, B, C, D, G, DI, CLK, LAT);
     CalcNextBlocks calcNextBlocks (cur_state, cur_tlx, cur_tly, cur_rotate, 
                                     left_signal, right_signal, down_signal, rotate_signal,
                                      next_tlx, next_tly, next_rotate);
     CalcCurBlocks calcCurXy (cur_tlx, cur_tly, cur_shape, cur_rotate, 
-                                 cur_xy1, cur_xy2, cur_xy3, cur_xy4,
-                                 cur_brx, cur_bry);
+                             cur_xy1, cur_xy2, cur_xy3, cur_xy4,
+                             cur_brx, cur_bry);
     CalcNextXy calcNextXy (next_tlx, next_tly, cur_shape, next_rotate, 
-                                  next_xy1, next_xy2, next_xy3, next_xy4,
-                                  next_brx, next_bry);
+                           next_xy1, next_xy2, next_xy3, next_xy4,
+                           next_brx, next_bry);
+    bcd_decoder(.clock(clk_5mhz), .x0(ones_score), .x1(tens_score),
+                .x2(hundreds_score), .x3(thousands_score),
+                .an(an), .seg(seg));
     
     always @(posedge clk_5mhz) begin
         if (cur_state == PLAYING) begin
+            if (game_over) begin
+                cur_state <= DEAD;
+            end
+            else if (stop) begin
+                cur_state <= STOPPING;
+            end
             if (left_signal) begin
                 if (collide_with_left_wall || collide_with_fallen_blocks) begin
                 end
@@ -205,6 +248,7 @@ module Tetris(
             if (row_to_clear == 0) begin
                 fallen_blocks[0+:16] <= 0;
                 cur_state <= PLAYING;
+                GetScore();
             end
             else begin
                 fallen_blocks[row_to_clear_start+:16] <= fallen_blocks[(row_to_clear_start - 1)-:16];
@@ -218,6 +262,16 @@ module Tetris(
                 cur_state <= PLAYING;
             end
             else cur_tly <= cur_tly + 1;
+        end
+        else if (cur_state == STOPPING) begin
+            if (!stop)
+                cur_state <= PLAYING;
+        end
+        else if (cur_state == DEAD) begin
+            if (reset) begin
+                ResetGame();
+                cur_state <= PLAYING;
+            end  
         end
     end
     
